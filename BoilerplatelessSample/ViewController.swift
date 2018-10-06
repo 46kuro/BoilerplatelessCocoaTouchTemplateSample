5ed84e6cfd8fc017c5bb0a19af1d94b8d9b6d461
shinji46kuro <shinji.46.kuro@gmail.com>
2018-10-06 15:38:18 +0900

    Initial Commit

---
+++ BoilerplatelessSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoilerplatelessSample
+//
+//  Created by Shinji Kurosawa on 2018/10/06.
+//  Copyright © 2018 Shinji Kurosawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
